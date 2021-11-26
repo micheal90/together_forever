a46a5c73dc0d5d21862bb318f4a0abfd6542d3b4
micheal90 <michealhana@gmail.com>
2021-11-26 18:38:41 +0200

    initiate my order screen

---
+++ lib/views/my_order_screen/my_order_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MyOrderScreen extends StatelessWidget {
+  const MyOrderScreen({Key? key}) : super(key: key);
+  static const routeName = '/myOrderScreen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
